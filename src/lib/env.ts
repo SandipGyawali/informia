b22ac76dc55bd35ab682504d2fc489b133873d1b
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-09-08 22:34:08 +0545

    env, utils integration

---
+++ src/lib/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const ENV = createEnv({
+  server: {
+    OPENAI_API_KEY: z.string(),
+    GEMINI_API_KEY: z.string(),
+  },
+  client: {},
+  runtimeEnv: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+  },
+});
